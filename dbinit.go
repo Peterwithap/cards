ebc3003f21ac9c66735317b502376b80e3fa2379
Peterwithap <91854237+Peterwithap@users.noreply.github.com>
2021-10-04 14:32:22 +1300

    Create dbinit.go

---
+++ dbinit.go
@@ -0,0 +1,25 @@
+package github.com/Peterwithap/cards
+
+import (
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
+	"log"
+	)
+
+func initdb() *sql.DB{
+    cfg := mysql.Config{
+        User:   "fela",
+        Passwd: "africa70",
+        Net:    "tcp",
+        Addr:   "127.0.0.1:3306",
+        DBName: "iconEditor",
+    }
+	// Get a database handle.
+    var err error
+    var db *sql.DB
+    db, err = sql.Open("mysql", cfg.FormatDSN())
+    if err != nil {
+        log.Fatal(err)
+    }
+    return db
+}
