eae6205651aafd27c5f4d288568c9447002406e4
Peterwithap <91854237+Peterwithap@users.noreply.github.com>
2021-10-04 14:30:15 +1300

    Create icons.go

---
+++ icons.go
@@ -0,0 +1,68 @@
+package main
+  
+import (
+        "fyne.io/fyne/v2"
+    "fyne.io/fyne/v2/app"
+        "fyne.io/fyne/v2/container"
+        "fyne.io/fyne/v2/widget"
+        "fyne.io/fyne/v2/canvas"
+        "database/sql"
+        "github.com/go-sql-driver/mysql"
+    "log"
+    "fyne.io/fyne/v2/dialog"
+        "github.com/Peterwithap/cardEditor/dbinit.go"
+)
+
+var db *sql.DB
+
+
+func main() {
+        info :="aall good"
+
+        myApp := app.New()
+        myWindow := myApp.NewWindow("t")
+         content:= initWindow()
+
+        db = initdb()
+
+
+        myWindow.SetContent(content)
+        dialog.ShowInformation("Check 12", info, myWindow)
+
+
+        myWindow.ShowAndRun()
+
+}
+
+func initWindow() *fyne.Container{      // pass default content later
+        label1 := "Set name"
+        image := canvas.NewImageFromFile("/Users/peterlorimer/iconImages/original/happy.png")
+        image.FillMode = canvas.ImageFillOriginal
+        //image.SetMinSize(fyne.Size(200))
+        imageCont := container.NewVBox(
+                image,
+                widget.NewLabel("36                                x"), //manual widen Box
+        )
+
+        content1 := container.NewVBox(
+                container.NewHBox(
+                        widget.NewLabel("Icon Editor"),
+                        widget.NewLabel(label1),
+                ),
+                container.NewHBox(
+                        container.NewVBox(
+                                widget.NewButton("DO something", func() {}),
+                                widget.NewLabel("Label 1"),
+                                widget.NewLabel("Label 2"),
+                                widget.NewLabel("Label 3"),
+                                widget.NewLabel("Label 4"),
+                        ),
+                        imageCont,
+                ),
+        )
+
+//      content1.Add(widget.NewButton("Add more items", func() {
+//              content1.Add(widget.NewLabel("Added"))
+//      }))
+        return content1
+}
